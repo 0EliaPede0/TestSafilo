0c4d0507b6104989360d1b2f81397d7e234c946c
Elia Pede <38972600+0EliaPede0@users.noreply.github.com>
2023-08-31 12:20:00 +0200

    Aggiunta finto slide con 3 iframe

---
+++ js.js
@@ -0,0 +1,62 @@
+let giri = {};
+let timer = 6000;
+
+// let siti = [
+//     "https://sun.fedabo.com/Home/TestSafiloPUno",
+//     "https://sun.fedabo.com/Home/TestSafiloPDue",
+//     "https://sun.fedabo.com/Home/TestSafiloPTre"
+// ];
+
+document.addEventListener("DOMContentLoaded", function () {
+    // giri = document.getElementsByClassName("gira");
+    giri = 3;
+    // startSlide(2);
+    startSlideDue(2);
+});
+
+function startSlide(numeroAttuale) {
+    setTimeout(() => {
+        let elemntiGruppo = document.getElementsByClassName("gira" + (numeroAttuale - 1));
+        for (let index = 0; index < elemntiGruppo.length; index++) {
+            elemntiGruppo[index].setAttribute("hidden", "");
+        }
+        elemntiGruppo = document.getElementsByClassName("gira" + numeroAttuale);
+        for (let index = 0; index < elemntiGruppo.length; index++) {
+            elemntiGruppo[index].removeAttribute("hidden", "");
+        }
+        if (numeroAttuale != giri) {
+            startSlide(numeroAttuale + 1);
+        }
+        else {
+            setTimeout(() => {
+                window.location.reload();
+            }, timer);
+        }
+    }, timer);
+}
+
+function startSlideDue(numeroAttuale) {
+    setTimeout(() => {
+        let elemntiGruppo = document.getElementsByClassName("gira" + (numeroAttuale - 1));
+        for (let index = 0; index < elemntiGruppo.length; index++) {
+            elemntiGruppo[index].setAttribute("hidden", "");
+        }
+        elemntiGruppo = document.getElementsByClassName("gira" + numeroAttuale);
+        for (let index = 0; index < elemntiGruppo.length; index++) {
+            elemntiGruppo[index].removeAttribute("hidden", "");
+        }
+        if (numeroAttuale != giri) {
+            startSlide(numeroAttuale + 1);
+        }
+        else {
+            let elemntiGruppo = document.getElementsByTagName("iframe");
+            for (let index = 0; index < elemntiGruppo.length; index++) {
+                elemntiGruppo[index].setAttribute("hidden", "");
+            }
+            startSlide(2);
+            // setTimeout(() => {
+            //     window.location.reload();
+            // }, timer);
+        }
+    }, timer);
+}
